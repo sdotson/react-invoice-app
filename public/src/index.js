6c52fb7b898bfd5994d1773e11b041899234c6e9
sdotson <stu.dotson@gmail.com>
2017-04-05 18:11:28 -0400

    remove unused middleware

--- public/src/index.js
+++ public/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
-import Async from './middlewares/async';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
